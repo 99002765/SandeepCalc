330be1fff88d5608a470eca07e98e699726b74af
99002765 <72502346+99002765@users.noreply.github.com>
2020-10-08 09:39:00 +0530

    Create calculator_operations.h

---
+++ Implementation/inc/calculator_operations.h
@@ -0,0 +1,16 @@
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+int add(int operand1, int operand2);
+int subtract(int operand1, int operand2);
+int multiply(int operand1, int operand2);
+int divide(int operand1, int operand2);
+int modulus(int operand1, int operand2);
+int factorial(int operand1);
+int permutation(int operand1, int operand2);
+int combination(int operand1, int operand2);
+
+#endif
